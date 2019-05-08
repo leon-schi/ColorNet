5c1b14d7b2b2181bd8c87dc731f6d64646d93a5c
Leon Schiller <leon.schiller99@gmail.com>
2019-05-08 14:53:11 +0200

    added config filr

---
+++ model/config.py
@@ -0,0 +1,14 @@
+CONFIG = {
+    'model_architecture': {
+        'num_poolings': 2,
+        'num_filters': 16
+    },
+    'training_config': {
+        'training_data_dir': 'training-data',
+        'checkpoint-dir': 'model/checkpoints',
+        'num_epochs': 4,
+        'num_iterations': 100,
+        'learning_rate': 0.001,
+        'learning_rate_decay': 0.01
+    }
+}
